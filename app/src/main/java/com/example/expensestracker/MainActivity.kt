ce3197ce1f0a44eb170453d2bfb6be3f08d0ac17
NikolaMomchilovic <nikolamomcilovic@coherentsolutions.com>
2023-02-02 17:23:01 +0100

    Added fingerprint authentication.

--- app/src/main/java/com/example/expensestracker/MainActivity.kt
+++ app/src/main/java/com/example/expensestracker/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.expensestracker
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
@@ -8,6 +10,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -15,5 +20,13 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.container).navigateUp()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        sharedPreferences =
+            getSharedPreferences("sharedPref", Context.MODE_PRIVATE)!!
+        sharedPreferences.edit().putBoolean("isAuthorized", false).apply()
     }
 }
